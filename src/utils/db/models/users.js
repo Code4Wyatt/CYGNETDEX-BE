9f3f406e654f6296c1cbe35f5c3093f001d41b90
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2022-09-24 20:11:16 +0100

    Added models

---
+++ src/utils/db/models/users.js
@@ -0,0 +1,39 @@
+import sequelize from "../index.js";
+
+import s from "sequelize";
+
+const { DataTypes } = s;
+
+const Users = sequelize.define("Users", {
+    userId: {
+        primaryKey: true,
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+    },
+    legalName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+    },
+    dateOfBirth: {
+        type: DataTypes.DATEONLY,
+        allowNull: false,
+    },
+    phoneNumber: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+    },
+    governmentId: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+    },
+    userAddress: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+    }
+});
+
+export default Users;
