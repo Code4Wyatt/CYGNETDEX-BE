895ffdc56d41e20770c2f75a6bdb9ec2292c9c8e
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2023-09-27 17:51:01 +0100

    add /swap xumm payload endpoint

--- src/routes/xumm/index.js
+++ src/routes/xumm/index.js
@@ -142,4 +142,31 @@ xummRouter.post(
   }
 );
 
+xummRouter.post('/swap', async (req, res) => {
+  const { destination, amount } = req.body;
+  
+  if (!destination || !amount) {
+    return res.status(400).json({ error: 'Destination and Amount are required.' });
+  }
+
+  try {
+    const request = {
+      TransactionType: 'Payment',
+      Destination: destination,
+      Amount: amount
+    };
+
+    const sdk = new XummSdk('3fc00c1e-2478-4696-810d-f6e432f64b96', '6ba46924-3662-473b-8ddd-97ef60a280da');
+    const payload = await sdk.payload.create(request, true);
+
+    console.log('payload PAYLOAD', payload);
+
+    return res.json(payload);
+  } catch (error) {
+    console.error('Error creating payload:', error);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
 export default xummRouter;
